88c9d5578a88fe9dc5fb57e953669d44ef2d19e7
joharasmus <joha.rasmus@gmail.com>
2023-02-08 19:30:19 +0100

    Initial commit

---
+++ foodBot.py
@@ -0,0 +1,56 @@
+import requests
+import discord
+from discord.ext import commands
+from datetime import date
+from bs4 import BeautifulSoup
+
+
+def format_days(lines, week_num):
+    days = ["Måndag:", "Tisdag:", "Onsdag:", "Torsdag:", "Fredag:"]
+
+    for idx, item in enumerate(lines):
+        if item in days:
+            which_day = days.index(item) + 1
+            d = date.fromisocalendar(2023, week_num, which_day)
+            new_item = f"**{item[:-1]} {d.day}/{d.month}:**"
+            lines[idx] = new_item
+
+    days = '\n'.join(lines)
+    return days
+
+
+def get_week():
+    resp = requests.get("https://adelfors.nu/folkhoegskolan/veckans-matsedel/")
+    soup = BeautifulSoup(resp.content, 'html.parser')
+    lunch_menu_all_parts = soup.find_all(id="Header3")
+    lunch_menu_flat = [item for part in lunch_menu_all_parts for item in part]
+    lunch_menu = [item.text for item in lunch_menu_flat]
+    lunch_menu = [item for item in lunch_menu if item != ""]
+    week_line = [item for item in lunch_menu if 'vecka' in item][0]
+    week_num = int(''.join(s for s in week_line if s.isdigit()))
+    first_day_line = lunch_menu.index('Måndag:')
+    lunch_menu = lunch_menu[first_day_line:]
+    lunch_menu.insert(0, 'Detta är veckans luncher:')
+    lunch_menu.insert(1, '')
+
+    days = format_days(lunch_menu, week_num)
+    return days
+
+
+if __name__ == '__main__':
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    bot = commands.Bot(command_prefix="!", intents=intents)
+
+    @bot.event
+    async def on_ready():
+        print(f"{bot.user} is now online")
+
+    @bot.command(name="mat")
+    async def food(ctx):
+        response = get_week()
+        await ctx.send(response)
+
+    token = "MTA2MjgyNjY1OTE1OTQ3ODMzMw.GZN-AV.MpWuOkuqVjcg5n3-Z0Gb-jsgXoO5bjDfYdP6Uo"
+    bot.run(token)
---
+++ workBot.py
@@ -0,0 +1,93 @@
+import json
+
+import requests
+import discord
+from discord.ext import tasks
+from datetime import datetime
+from datetime import timedelta
+
+BASE_URL = 'https://jobstream.api.jobtechdev.se'
+STREAM_URL = f"{BASE_URL}/stream"
+
+JONKOPINGS_LAN = 'MtbE_xWT_eMi'
+
+YRKESGRUPP_2511 = 'UXKZ_3zZ_ipB'
+YRKESGRUPP_2512 = 'DJh5_yyF_hEM'
+YRKESGRUPP_2513 = 'Q5DF_juj_8do'
+YRKESGRUPP_2514 = 'D9SL_mtn_vGM'
+YRKESGRUPP_2515 = 'cBBa_ngH_fCx'
+YRKESGRUPP_2516 = 'BAeH_eg8_T2d'
+YRKESGRUPP_2519 = 'UxT1_tPF_Kbg'
+YRKESGRUPP_251 = [YRKESGRUPP_2511, YRKESGRUPP_2512, YRKESGRUPP_2513, YRKESGRUPP_2514,
+                  YRKESGRUPP_2515, YRKESGRUPP_2516, YRKESGRUPP_2519]
+YRKESGRUPP_3511 = '13md_uyV_BNG'
+YRKESGRUPP_3512 = 'hmaC_cfi_UKg'
+YRKESGRUPP_3513 = 'MYAz_x9m_2LJ'
+YRKESGRUPP_3514 = 'VCpu_5EN_bBt'
+YRKESGRUPP_3515 = 'Fv7d_YhP_YmS'
+YRKESGRUPP_351 = [YRKESGRUPP_3511, YRKESGRUPP_3512, YRKESGRUPP_3513, YRKESGRUPP_3514, YRKESGRUPP_3515]
+
+YRKEN = YRKESGRUPP_251
+YRKEN.extend(YRKESGRUPP_351)
+
+DATE_FORMAT = '%Y-%m-%dT%H:%M:%S'
+
+
+def matching_date(ad, date):
+    pub_date = ad['publication_date']
+    pub_date = datetime.strptime(pub_date, DATE_FORMAT)
+    return pub_date.date() == date.date()
+
+
+def filter_ads(ads_list, date):
+    filtered = [ad for ad in ads_list if not ad['removed']]
+    filtered = [ad for ad in filtered if matching_date(ad, date)]
+    return filtered
+
+
+def fetch_stream(timestamp: datetime):
+    td = timedelta(days=1)
+    prev_day = timestamp - td
+    params = {
+        'date': prev_day.strftime(DATE_FORMAT),
+        'updated-before-date': timestamp.strftime(DATE_FORMAT),
+        'location-concept-id': JONKOPINGS_LAN,
+        'occupation-concept-id': YRKEN
+    }
+    response = requests.get(STREAM_URL, params=params)
+    return response.content
+
+
+def get_ads():
+    start_of_day = datetime.now().replace(hour=0, minute=0, second=0)
+    td = timedelta(days=1)
+    start_of_yesterday = start_of_day - td
+    stream = fetch_stream(start_of_day)
+    all_ads = json.loads(stream)
+    relevant_ads = filter_ads(all_ads, start_of_yesterday)
+    return relevant_ads
+
+
+if __name__ == '__main__':
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @tasks.loop(hours=24)
+    async def scheduled_loop():
+        all_channels = list(client.get_all_channels())
+        af_channel = discord.utils.get(all_channels, name='platsbanken-it-spåret')
+
+        for ad in get_ads():
+            desc = ad['headline']
+            link = ad['webpage_url']
+            msg = f'**{desc}**\n<{link}>'
+            await af_channel.send(msg)
+
+    @client.event
+    async def on_ready():
+        print("Worky is starting")
+        scheduled_loop.start()
+
+    token = 'MTA2NjMxMjA4NzE2NjUyNTQ4MQ.G2ZPYd.L2LeOuEwHFxYv8c8O3DM_UwoC3U6tc61-cwFqk'
+    client.run(token=token)
