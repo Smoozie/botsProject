92deeec894f7d42ea9d6d3e153ff14284eba9ff0
Rasmus Johansson <joha.rasmus@gmail.com>
2023-02-25 18:58:49 +0100

    Another change for python3.10

--- start_all.sh
+++ start_all.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/bash
 
-nohup python3.9 foodBot.py >/dev/null 2>&1 &
-nohup python3.9 workBot.py >/dev/null 2>&1 &
+nohup python3.10 foodBot.py >/dev/null 2>&1 &
+nohup python3.10 workBot.py >/dev/null 2>&1 &
